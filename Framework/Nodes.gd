6ddb83f786283ede04226bcc181d132dfaa3d1b5
yaelatletl <yael.atletl@gmail.com>
2019-03-07 13:12:33 -0600

    Node colour mismatch fixed

--- Framework/Nodes.gd
+++ Framework/Nodes.gd
@@ -1,6 +1,6 @@
 extends Node
 
 const VECTOR = Color(0.5,0.2,0.2,1)
-const INT = Color(0.8,0.8,1,1)
+const INT = Color(0.8,0.3,1,1)
 const BOOL = Color(1,1,1,1)
 const FLOAT = Color(0,1,0,1)
--- Framework/Nodes/GraphNode.gd
+++ Framework/Nodes/GraphNode.gd
@@ -3,7 +3,6 @@ extends GraphNode
 
 func _ready():
 	set_slot(0, true, TYPE_BOOL, Nodes.BOOL, false,0, Color(0,1,0,1))
-	set_slot(1, true, TYPE_REAL, Nodes.FLOAT, false, 0 , Color(0,1,0,1))
-	set_slot(2, false, 0, Color(1,1,1,1), true, TYPE_BOOL, Nodes.BOOL)
-	set_slot(3, false, 0, Color(1,1,1,1), true, TYPE_BOOL, Nodes.BOOL)
-	set_slot(4, false, 0, Color(1,1,1,1), true, TYPE_BOOL, Nodes.BOOL)
+	set_slot(1, true, TYPE_REAL, Nodes.FLOAT, true, TYPE_BOOL, Nodes.BOOL)
+	set_slot(2, true, TYPE_REAL, Nodes.FLOAT, true, TYPE_BOOL, Nodes.BOOL)
+	set_slot(3, true, TYPE_REAL, Nodes.FLOAT, true, TYPE_BOOL, Nodes.BOOL)
