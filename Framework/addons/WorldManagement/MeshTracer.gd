3def2f1e4c5d6f65604d910a87503e46ee2c26a6
yaelatletl <yael.atletl@gmail.com>
2019-04-02 10:27:03 -0600

    MeshTracer in progress

--- Framework/addons/WorldManagement/MeshTracer.gd
+++ Framework/addons/WorldManagement/MeshTracer.gd
@@ -1,12 +1,17 @@
 extends MeshInstance
 
 func _ready():
-	pass # Replace with function body.
+	pass 
 func routine():
-	if $Floor_center.is_colliding(): #The Tracer must be hitting the floor!
+	if $Floor_center.is_colliding(): #The Tracer must be hitting the floor! (Most of the time, there's an exception to this rule)
 		pass
 	#First we spawn and search for a corner
 	#What is a corner?
 	#A corner can either: Have no walls around it
 	#Or Have some walls around it
+		#Corner Case 1: A wall-less corner, from a plane in the space, won't have any wall colliders, but will have two floor colliders
+	#We will register the discovery of a corner
+	#If we move up 3 meters the tracer and every floor collider is colliding, it means that we're on the roof. 
+	#If we move up 3 meters and there's at least three free colliders, that means we have an elevator. We have to find a better way to calculate this. 
+	
 	pass
