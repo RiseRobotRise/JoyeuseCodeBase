2a03c04a0680997d265ef54f4596257642636b1a
yael <yael.atletl@gmail.com>
2020-11-02 21:33:10 -0600

    Rebuilt Marathon-Styled HUD for dynamic resolution

--- assets/radar/Player_HUD.gd
+++ assets/radar/Player_HUD.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Control
 
 # class member variables go here, for example:
 # var a = 2
@@ -7,23 +7,21 @@ extends CanvasLayer
 var health = 100
 var oxygen = 1000
 var debug = false
-var x1
-var x2
-var x3
+onready var x1 : Control = get_node("VBoxContainer/HBoxContainer/HealthRadar/Container/x1")
+onready var x2 : Control = get_node("VBoxContainer/HBoxContainer/HealthRadar/Container/x2")
+onready var x3 : Control = get_node("VBoxContainer/HBoxContainer/HealthRadar/Container/x3")
+onready var Ox : Control = get_node("VBoxContainer/HBoxContainer/HealthRadar/Container/Oxygen")
 export(NodePath) var radarpath = null
 
 
 func _ready():
-	get_node("Health|Radar/Radar").Ppath = radarpath
+	get_node("VBoxContainer/HBoxContainer/HealthRadar/Container/Radar").Ppath = radarpath
 	# Called every time the node is added to the scene.
 	# Initialization here
 	#print(get_viewport().size)
 	get_tree().get_root().connect("size_changed", self, "myfunc")
-	x1 = get_node("Health|Radar/x1")
-	x2 = get_node("Health|Radar/x2")
-	x3 = get_node("Health|Radar/x3")
-	scale.x = (get_viewport().size.x/1920)
-	scale.y = (get_viewport().size.y/1080)
+#	scale.x = (get_viewport().size.x/1920)
+#	scale.y = (get_viewport().size.y/1080)
 	if health >= 101:
 		x2.visible = true
 	if health >= 201:
@@ -33,8 +31,8 @@ func _ready():
 	
 	
 func myfunc():
-	scale.x = (get_viewport().size.x/1920)
-	scale.y = (get_viewport().size.y/1080)
+#	scale.x = (get_viewport().size.x/1920)
+#	scale.y = (get_viewport().size.y/1080)
 	#print("Resizing: ", get_viewport().size)
 	pass
 
@@ -87,48 +85,48 @@ func change_health():
 	
 	if health >= 1 and health <= 100:
 		#get_node("Health|Radar/x1/T_health").interpolate_property(x1,"indicator_value", null ,float(health)/100 ,1.5,Tween.TRANS_LINEAR,Tween.EASE_OUT,0)
-		get_node("Health|Radar/x1").indicator_value = float(health)/100
+		x1.indicator_value = float(health)/100
 	elif health <= 0:
-		get_node("Health|Radar/x1").indicator_value = 0
+		x1.indicator_value = 0
 		#get_node("Health|Radar/x1/T_health").interpolate_property(x1,"indicator_value", null ,0  ,1.5,Tween.TRANS_LINEAR,Tween.EASE_OUT,0)
 	elif health >= 100:
-		get_node("Health|Radar/x1").indicator_value = 1
+		x1.indicator_value = 1
 		#get_node("Health|Radar/x1/T_health").interpolate_property(x1,"indicator_value", null ,1 ,1.5,Tween.TRANS_LINEAR,Tween.EASE_OUT,0)
 
 	if health >= 101 and health <= 200:
-		get_node("Health|Radar/x2").visible = true
+		x2.visible = true
 		#get_node("Health|Radar/x2/T_health").interpolate_property(get_node("Health|Radar/x2"),"indicator_value", health ,float(health-100)/100 ,1,Tween.TRANS_LINEAR,Tween.EASE_OUT_IN,0)
-		get_node("Health|Radar/x2").indicator_value = float(health-100)/100
+		x2.indicator_value = float(health-100)/100
 	elif health <= 100:
 		#get_node("Health|Radar/x2/T_health").interpolate_property(get_node("Health|Radar/x2"),"indicator_value", health ,0 ,1,Tween.TRANS_LINEAR,Tween.EASE_OUT_IN,0)
-		get_node("Health|Radar/x2").indicator_value = 0
-		get_node("Health|Radar/x2").visible = false
+		x2.indicator_value = 0
+		x2.visible = false
 	elif health >= 200:
 		#get_node("Health|Radar/x2/T_health").interpolate_property(get_node("Health|Radar/x2"),"indicator_value", health ,1 ,1,Tween.TRANS_LINEAR,Tween.EASE_OUT_IN,0)
-		get_node("Health|Radar/x2").indicator_value = 1 
+		x2.indicator_value = 1 
 	if health >= 201 and health <= 300:
-		get_node("Health|Radar/x3").visible = true
-		get_node("Health|Radar/x3").indicator_value = float(health-200)/100
+		x3.visible = true
+		x3.indicator_value = float(health-200)/100
 	elif health <= 200:
-		get_node("Health|Radar/x3").indicator_value = 0
-		get_node("Health|Radar/x3").visible = false
+		x3.indicator_value = 0
+		x3.visible = false
 	elif health >= 300:
-		get_node("Health|Radar/x3").indicator_value = 1
+		x3.indicator_value = 1
 
 	if health <= 0:
-		get_node("Health|Radar/x1").indicator_value = 0
-		get_node("Health|Radar/x2").indicator_value = 0
-		get_node("Health|Radar/x3").indicator_value = 0
+		x1.indicator_value = 0
+		x1.indicator_value = 0
+		x3.indicator_value = 0
 	pass
 
 
 
 func change_oxygen(oxygen):
 	if oxygen >= 1 and oxygen <= 1000:
-		get_node("Health|Radar/Oxygen").indicator_value = float(oxygen)/1000
+		Ox.indicator_value = float(oxygen)/1000
 	elif oxygen <= 0:
-		get_node("Health|Radar/Oxygen").indicator_value = 0
+		Ox.indicator_value = 0
 	elif oxygen >= 1000:
-		get_node("Health|Radar/Oxygen").indicator_value = 1
+		Ox.indicator_value = 1
 
 	pass
